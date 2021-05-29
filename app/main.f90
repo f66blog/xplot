42ba68e739dc425840efef313d2cae727f24cf85
Your Name <you@example.com>
2021-05-29 10:40:34 +0900

    indent

--- app/main.f90
+++ app/main.f90
@@ -27,34 +27,30 @@ contains
     subroutine monte_carlo(fig, x, y)
         class(device_t), intent(in out) :: fig
         real, intent(in) :: x(:), y(:)
-
-      integer :: i, ix0, iy0, ix1, iy1, k
-      k = fig%nx
-      print *
-      print *, 'Monte Carlo: estimated pi =', 4.0 * count(x**2 + y**2 < 1.0) / size(x) 
-      call fig%init()
-      ! draw box
-      call fig%line0(0, 0, k-1, 0)
-      call fig%line0(0, 0, 0, k-1)
-      call fig%line0(0, k-1, k-1, k-1)
-      call fig%line0(k-1, 0, k-1, k-1)
-      ! draw 1/4 circle
-      ix0 = 0
-      iy0 = 0
-      do ix1 = 0, k - 1
-          iy1 = k - 1 - int(sqrt(real((k-1)**2 - ix1**2)))
-          call fig%line0(ix0, iy0, ix1, iy1)
-          ix0 = ix1
-          iy0 = iy1
-      end do           
-
-      do i = 1, n 
-          call fig%point(int(k * x(i)), int(k * y(i)))
-      end do 
-      call fig%show()
+        integer :: i, ix0, iy0, ix1, iy1, k
+        k = fig%nx
+        print *
+        print *, 'Monte Carlo: estimated pi =', 4.0 * count(x**2 + y**2 < 1.0) / size(x) 
+        call fig%init()
+        ! draw box
+        call fig%line0(0, 0, k-1, 0)
+        call fig%line0(0, 0, 0, k-1)
+        call fig%line0(0, k-1, k-1, k-1)
+        call fig%line0(k-1, 0, k-1, k-1)
+        ! draw 1/4 circle
+        ix0 = 0
+        iy0 = 0
+        do ix1 = 0, k - 1
+            iy1 = k - 1 - int(sqrt(real((k-1)**2 - ix1**2)))
+            call fig%line0(ix0, iy0, ix1, iy1)
+            ix0 = ix1
+            iy0 = iy1
+        end do           
+  
+        do i = 1, n 
+            call fig%point(int(k * x(i)), int(k * y(i)))
+        end do 
+        call fig%show()
     end subroutine monte_carlo
-
-
-    
 
 end program main
--- src/device.f90
+++ src/device.f90
@@ -1,45 +1,44 @@
 module device
     implicit none
-type, abstract :: device_t
-    integer :: nx, ny
-contains
-    procedure (device_init) , deferred, pass :: init     
-    procedure (device_point), deferred, pass :: point 
-    procedure (device_show) , deferred, pass :: show  
-    procedure (device_line0), deferred, pass :: line0
-    procedure (device_line) , deferred, pass :: line
-end type device_t
-
-abstract interface
-
-subroutine device_init(fig)
-    import :: device_t
-    class(device_t), intent(in out) :: fig 
-end subroutine device_init  
-
-subroutine device_point(fig, ix, iy)
-    import :: device_t
-    class(device_t), intent(in out) :: fig 
-    integer, intent(in) :: ix, iy
-end subroutine device_point
-
-subroutine device_show(fig)
-    import :: device_t
-    class(device_t), intent(in) :: fig
-end subroutine device_show    
-
-subroutine device_line0(fig, ix0, iy0, ix1, iy1)
-    import :: device_t
-    class(device_t), intent(in out) :: fig 
-    integer, intent(in) :: ix0, iy0, ix1, iy1
-end subroutine device_line0
-
-subroutine device_line(fig, x, y, ipen)
-    import :: device_t
-    class(device_t), intent(in out) :: fig
-    real, intent(in) :: x, y
-    integer, intent(in) :: ipen
-end subroutine device_line
-end interface
-
+    type, abstract :: device_t
+        integer :: nx, ny
+    contains
+        procedure (device_init) , deferred, pass :: init     
+        procedure (device_point), deferred, pass :: point 
+        procedure (device_show) , deferred, pass :: show  
+        procedure (device_line0), deferred, pass :: line0
+        procedure (device_line) , deferred, pass :: line
+    end type device_t
+    
+    abstract interface
+        subroutine device_init(fig)
+            import :: device_t
+            class(device_t), intent(in out) :: fig 
+        end subroutine device_init  
+        
+        subroutine device_point(fig, ix, iy)
+            import :: device_t
+            class(device_t), intent(in out) :: fig 
+            integer, intent(in) :: ix, iy
+        end subroutine device_point
+        
+        subroutine device_show(fig)
+            import :: device_t
+            class(device_t), intent(in) :: fig
+        end subroutine device_show    
+        
+        subroutine device_line0(fig, ix0, iy0, ix1, iy1)
+            import :: device_t
+            class(device_t), intent(in out) :: fig 
+            integer, intent(in) :: ix0, iy0, ix1, iy1
+        end subroutine device_line0
+        
+        subroutine device_line(fig, x, y, ipen)
+            import :: device_t
+            class(device_t), intent(in out) :: fig
+            real, intent(in) :: x, y
+            integer, intent(in) :: ipen
+        end subroutine device_line
+    end interface
+    
 end module device
